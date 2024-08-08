4c76f04f6be11afeee6fc0f8262ba2d8cc36403b
Aditya <aditya.2023@vitstudent.com>
2024-08-09 00:45:49 +0530

    Updated the firebase auth part from google only to email-password verification

--- Server/src/Firebase/auth.js
+++ Server/src/Firebase/auth.js
@@ -1,13 +1,63 @@
 import { auth } from "./firebase";
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    sendPasswordResetEmail,
+    sendEmailVerification,
+    updatePassword,
+    signInWithPopup,
+    GoogleAuthProvider,
+} from "firebase/auth";
 
-export const doSignInWithGoogle = async() => {
-    const provider = new GoogleAuthProvider();
-    const result = await signInWithPopup(auth,provider);
-
-    return result;
+export const doCreateUserWithEmailAndPassword = async (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
 };
 
-export const doSignOut = ()=> {
+export const doSignInWithEmailAndPassword = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+};
+
+export const doSignInWithGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+    const result = await signInWithPopup(auth, provider);
+    const user = result.user;
+
+    // add user to firestore
+};
+
+export const doSignOut = () => {
     return auth.signOut();
+};
+
+export const doPasswordReset = (email) => {
+    return sendPasswordResetEmail(auth, email);
+};
+
+export const doPasswordChange = (password) => {
+    return updatePassword(auth.currentUser, password);
+};
+
+export const doSendEmailVerification = () => {
+    return sendEmailVerification(auth.currentUser, {
+        url: `${window.location.origin}/home`,
+    });
+
+
+
+//OLD CODE
+
+    // import { auth } from "./firebase";
+    // import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+    // export const doSignInWithGoogle = async() => {
+    //     const provider = new GoogleAuthProvider();
+    //     const result = await signInWithPopup(auth,provider);
+
+    //     return result;
+    // };
+
+    // export const doSignOut = ()=> {
+    //     return auth.signOut();
+    // }; 
+
 };
