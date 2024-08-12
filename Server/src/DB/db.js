1f326caa4ccf105681e9f12f6ff102f44a56a78c
Ramakrishnan Ilanghovan <145325218+RK18113@users.noreply.github.com>
2024-08-12 20:46:02 +0530

    Updated the user schema

---
+++ Server/src/DB/db.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true,
+    },
+    lastname: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
+   
+});
+
+userSchema.pre('save', function (next) {
+    this.updatedAt = Date.now();
+    next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
