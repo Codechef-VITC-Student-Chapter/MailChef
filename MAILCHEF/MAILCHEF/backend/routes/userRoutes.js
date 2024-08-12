ae355029921e8f7534c775ce9d0f6cb3f9eb91d7
Ramakrishnan Ilanghovan <145325218+RK18113@users.noreply.github.com>
2024-08-12 20:36:50 +0530

    Delete MAILCHEF directory

--- MAILCHEF/MAILCHEF/backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Define your routes here
-router.get('/users', (req, res) => {
-    res.send('User route');
-});
-
-module.exports = router;
