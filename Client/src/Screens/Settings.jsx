0e4b77b075691b62585bf426d9324f4c6832edbe
dishadaniellol <142607124+dishadaniellol@users.noreply.github.com>
2024-08-13 23:35:03 +0530

    main page changes

---
+++ Client/src/Screens/Settings.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Settings() {
+  return (
+    <div>
+      <h1>Settings Page</h1>
+      <p>Your settings options will appear here.</p>
+    </div>
+  );
+}
+
+export default Settings;
