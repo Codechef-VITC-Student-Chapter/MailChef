bb02d1c41a49cf8818f540e8f9b85a29f5569b55
dishadaniellol <142607124+dishadaniellol@users.noreply.github.com>
2024-08-14 00:05:37 +0530

    main page changes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Mainpage from './Screens/Mainpage.jsx';
+import Saved from './Screens/Saved.jsx';
+import Templates from './Screens/Templates.jsx';
+import Download from './Screens/Download.jsx';
+import Settings from './Screens/Settings.jsx';
+
 function App() {
-  return <div className="text-red-950">hi</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Mainpage />} />
+        <Route path="/saved" element={<Mainpage content={<Saved />} />} />
+        <Route path="/templates" element={<Mainpage content={<Templates />} />} />
+        <Route path="/download" element={<Mainpage content={<Download />} />} />
+        <Route path="/settings" element={<Mainpage content={<Settings />} />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
