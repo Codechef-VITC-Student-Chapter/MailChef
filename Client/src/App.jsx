642e9a4d1230cc8b803cb30fa4f029a0f29b9886
Sushindh <146247085+Sushindh@users.noreply.github.com>
2024-08-14 17:53:14 +0530

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Sign from "./Screens/Signup.jsx"
+import Login from "./Screens/Login.jsx"
+
 function App() {
-  return <div className="text-red-950">hi</div>;
+  return(
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Login />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Signup" element={<Sign />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-export default App;
+export default App
